9d9eee387f2d11ddc01deb13a758039a6427d637
Logan Bek <loganbek@gmail.com>
2020-08-17 14:49:22 -0700

    meeting research

--- streamlineTools/popup.js
+++ streamlineTools/popup.js
@@ -15,13 +15,14 @@ unloadButton.onclick = function (params) {
   //TODO: BML code selector
   let unloaded = true;
 
+  // innerhtml
   //*[@id="selection_field_text"]/strong/text()[1]
   // let code = document.getElementsByClassName('bml');
   // /html/body/div[1]/div[3]/div[2]/div[7]/strong/text()[1]
 
 
-
-  let code = document.querySelector('*[@id="selection_field_text"]/strong/text()[1]');
+  #selection_field > strong
+  let code = document.getElementById('selection_field_text');
   console.log(code);
 
   // HTMLCollection
