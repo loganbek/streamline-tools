6398e50c547bb7ea28697d28828cd8ab4c99d07f
Logan Bek <loganbek@gmail.com>
2020-08-25 15:25:54 -0700

    working messagePassing?

--- streamlineTools/popup.js
+++ streamlineTools/popup.js
@@ -10,19 +10,16 @@ let unloadButton = document.getElementById('unload');
 
 let loadButton = document.getElementById('load');
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
-
 unloadButton.onclick = function (params) {
   console.log("unload clicked");
   //TODO: BML code selector
   let unloaded = true;
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
+      console.log(response.farewell);
+    });
+  });
 
   // jsonPath(jsonRespStr, "$.widget.items[1].component.widget.items[1].component.widget.items[0].component.data");
 
