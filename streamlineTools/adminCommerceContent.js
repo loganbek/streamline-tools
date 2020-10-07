83aca179f89f6b7de127b6689e183744c3210ae5
Logan Bek <loganbek@gmail.com>
2020-10-07 10:04:44 -0700

    init admin comm inject

---
+++ streamlineTools/adminCommerceContent.js
@@ -0,0 +1,42 @@
+// ADMIN COMMERCE CONTENT
+
+function injectJs(link) {
+    let scr = document.createElement('script');
+    scr.type = "text/javascript";
+    scr.src = link;
+    document.getElementsByTagName('head')[0].appendChild(scr);
+}
+
+injectJs(chrome.extension.getURL('adminCommerceInjected.js'));
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        let filename = document.getElementById('variableName').value;
+        if (filename === "") {
+            filename = "nofilename";
+        }
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        console.log(request.greeting ?
+            "greeting: " + request.greeting :
+            "nogreeting");
+        if (request.greeting == "unload") {
+            let unloadEvent = new CustomEvent("unloadCode", { detail: request.code });
+            window.dispatchEvent(unloadEvent);
+            if (!code.startsWith(commentHeader)) {
+                code = commentHeader + "\n\n" + code;
+            }
+            sendResponse({
+                filename: filename,
+                code: code
+            });
+        } else if (request.greeting == "load") {
+            let loadEvent = new CustomEvent("loadCode", { detail: request.code });
+            window.dispatchEvent(loadEvent);
+        } else if (request.greeting == "filename") {
+            sendResponse({
+                filename: filename
+            });
+        }
+    });
---
+++ streamlineTools/adminCommerceInjected.js
@@ -0,0 +1,20 @@
+//ADMIN COMMERCE INJECT
+
+window.addEventListener('load', function() {
+    main();
+})
+
+// window.addEventListener('unloadCode', function(evt) {
+//         let event = new CustomEvent("PassCodeToBackground", { detail: frame_bm_script.editArea.textarea.value });
+//         window.dispatchEvent(event);
+//     })
+//     //Listen for the load code event
+// window.addEventListener("loadCode", function(evt) {
+//     code = evt.detail;
+//     frame_bm_script.editArea.textarea.value = code;
+//     frame_bm_script.editArea.textareaFocused = true;
+
+//     //Perform Validation
+//     // document.getElementById('ext-gen22').click();
+//     document.getElementsByClassName('bmx-spellcheck')[0].click();
+// }, false);
