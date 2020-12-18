02ab96f8aa2b279d7413adaf882e825ab0541d48
Logan Bek <loganbek@gmail.com>
2020-12-18 09:06:05 -0800

    adminConfig simulateClick function

--- streamlineTools/adminConfigContent.js
+++ streamlineTools/adminConfigContent.js
@@ -416,3 +416,19 @@ var getParentsUntil = function(elem, parent, selector) {
 // var parentsUntilByFilter = getParentsUntil(elem, '.some-class', '[data-something]');
 // var allParentsUntil = getParentsUntil(elem);
 // var allParentsExcludingElem = getParentsUntil(elem.parentNode);
+
+/**
+ * Simulate a click event.
+ * @public
+ * @param {Element} elem  the element to simulate a click on
+ */
+var simulateClick = function(elem) {
+    // Create our event (with options)
+    var evt = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+    // If cancelled, don't dispatch our event
+    var canceled = !elem.dispatchEvent(evt);
+};
