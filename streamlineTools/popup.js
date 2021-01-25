efbb6cbc66e9e7e1cfad5895bec1969e8616c498
Logan Bek <loganbek@gmail.com>
2021-01-25 10:20:08 -0800

    unused/comment rm

--- streamlineTools/popup.js
+++ streamlineTools/popup.js
@@ -20,22 +20,10 @@ let loadTestButton = document.getElementById('loadTest');
 let optionsButton = document.getElementById('options');
 let logsButton = document.getElementById('logs');
 
-// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-//     document.write(`<h3>The tabs you're on are:</h3>`);
-//     document.write('<ul>');
-//     for (let i = 0; i < tabs.length; i++) {
-//       document.write(`<li>${tabs[i].url}</li>`);
-//     }
-//     document.write('</ul>');
-//   });
-
 // CHROME TABS
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     let tab = tabs[0];
-    // console.log("TAB");
-    // alert(tab);
     let url = tab.url;
-    // console.log(tab.url);
     let full = url;
     let parts = full.split('.');
     let sub = parts[0];
@@ -52,53 +40,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     console.log(bmSiteType);
     if (url !== undefined) {
 
-        //BML SITE TYPE TODO
-        // COMMERCE + UTIL HEADER
-        // .innerHTML.includes("Util");
-
-
-
-        // header = document.querySelector(".x-panel-header-text");
-        // isUtil = header.innerHTML.includes("Util");
-        // isCommerce = header.innerHTML.includes("Commerce");
-
-        // if (header && header.innerHTML.includes("Commerce")) {
-        //     bmSiteType = "commerce";
-        // } else if (header && header.innerHTML.includes("Util")) {
-        //     bmSiteType = "util"
-        // } else {
-        //     bmSiteType = "configuration"
-        // }
-
-        // if (document.querySelector(".xpanel-header-text")) {
-        //     header = document.querySelector(".x-panel-header-text");
-        //     isUtil = header.innerHTML.includes("Util");
-        //     isCommerce = header.innerHTML.includes("Commerce");
-        //     if (isCommerce) {
-        //         bmSiteType = "commerce";
-        //     } else if (isUtil) {
-        //         bmSiteType = "util";
-        //     } else {
-        //         bmSiteType = "configuration"
-        //     }
-        //     // TODO fix configuration
-        // }
-
-        //TEST BML DISABLING
+        // UNLOAD/LOAD TEST BML DISABLING
         if (url.includes("bigmachines.com/admin/commerce/rules") || url.includes("bigmachines.com/admin/configuration/rules") || url.includes("bigmachines.com/admin/commerce/actions")) {
-
-            // alert(url.includes("bigmachines.com/admin/commerce/rules/edit_rule_inputs.jsp"));
-            // INITIAL content.js LOADING
-            // executeContentScript("adminCommerceContent.js");
-            // unloadTestButton.style.visibility = "hidden";
-            // loadTestButton.style.visibility = "hidden";
             unloadTestButton.disabled = true;
             loadTestButton.disabled = true;
         }
-        // if (url.includes("bigmachines.com/admin/commerce/rules") || url.includes("bigmachines.com/admin/commerce/actions")) { // <-- maybe break these up into adminCommerceRulesContent + adminCommerceActionsContent; 
-        //     executeContentScript("adminCommerceContent.js");
-        // }
-        if (url.includes("bigmachines.com/admin/commerce/rules/edit_rule_inputs.jsp")) { // <-- need to fix this
+        if (url.includes("bigmachines.com/admin/commerce/rules/edit_rule_inputs.jsp")) {
             executeContentScript("adminCommerceActionsContent.js");
         } else if (url.includes("bigmachines.com/admin/commerce/rules")) {
             executeContentScript("adminCommerceRulesContent.js")
@@ -118,42 +65,17 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     }
 });
 
+// CS
 function executeContentScript(contentScriptName) {
     chrome.tabs.executeScript({
         file: contentScriptName
     });
 }
 
-// TODO: LOG LINKING
+// TODO: EXTERNAL LOG LINKING
 logsButton.disabled = true;
 
-// chrome.tabs.query({
-//     active: true,
-//     lastFocusedWindow: true
-// }, function(tabs) {
-//     // and use that tab to fill in out title and url
-//     var tab = tabs[0];
-//     console.log("url :", tab.url);
-//     // alert(tab.url);
-//     url = tab.url;
-// });
-
-// alert(url);
-
-// TEST BUTTON HIDING
-// if (url !== undefined) {
-//     alert(url.includes("bigmachines.com/admin/commerce/rules/edit_rule_inputs.jsp"));
-//     if (url.includes("bigmachines.com/admin/commerce/rules/edit_rule_inputs.jsp")) {
-//         unloadTestButton.style.visibility = "hidden";
-//         loadTestButton.style.visibility = "hidden";
-//         unloadTestButton.disabled = true;
-//         loadTestButton.disabled = true;
-//     }
-// }
-
-// unloadTestButton.disabled = true;
-// loadTestButton.disabled = true;
-
+// DOWNLOAD FILENAME
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
     suggest({
         filename: "bigmachines/" + bmSiteSubDomain + "/" + bmSiteType + "/" + item.filename,
@@ -164,101 +86,15 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 // UNLOAD ONCLICK
 unloadButton.onclick = function(params) {
     console.log("unload clicked");
-    // if (document.querySelector(".xpanel-header-text")) {
-    //     header = document.querySelector(".x-panel-header-text");
-    //     isUtil = header.innerHTML.includes("Util");
-    //     isCommerce = header.innerHTML.includes("Commerce");
-    //     if (isCommerce) {
-    //         bmSiteType = "commerce";
-    //     } else if (isUtil) {
-    //         bmSiteType = "util";
-    //     } else {
-    //         bmSiteType = "configuration"
-    //     }
-    //     // TODO fix configuration
-    // }
     let unloaded = true;
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { greeting: "unload" }, function(response) {
-            //             console.log(response.filename);
-            //             console.log(response.code);
-
-            //             WIP COMMERCE RULES FN
-            // if (tabs[0].url.includes("bigmachines.com/admin/commerce/rules")) {
-            //     chrome.storage.sync.get(['commerceFileName'], function(result) {
-            //         if (result.commerceFileName !== undefined) {
-            //             console.log("I am retrieved!!");
-            //             console.log(result.commerceFileName);
-            //             console.log(tabs[0].document);
-            //             if (tabs[0].title.includes("After")) {
-            //                 console.log("AFTER POPUP");
-            //                 saveText(result.commerceFileName + ".afterFormulas.bml", response.code);
-            //             } else if (tabs[0].title.includes("Before")) {
-            //                 console.log("BEFORE POPUP");
-            //                 saveText(result.commerceFileName + ".beforeFormulas.bml", response.code);
-            //             }
-
-            //             // response.filename = result.commerceFileName;
-            //             saveText(result.commerceFileName + ".bml", response.code);
-            //         }
-            //     });
-            // } else 
-
             if (response.code && response.filename) {
                 saveText(response.filename + ".bml", response.code);
             }
         });
     });
-
-    // var commerceRuleTabID;
-    // var commerceRuleTabIndex;
-    // // SEARCH FOR COMMERCE RULE TAB
-    // chrome.tabs.query({ currentWindow: true }, function(result) {
-    //     console.log("RESULT");
-    //     console.log(result);
-    //     result.forEach(function(tab) {
-    //         //         do stuff here;
-    //         console.log("TABID");
-    //         console.log(tab.id);
-    //         console.log("TABNAME");
-    //         console.log(tab.name);
-    //         console.log("TABURL");
-    //         console.log(tab.url);
-    //         console.log("TABINDEX");
-    //         console.log(tab.index);
-    //         // if https://devmcnichols.bigmachines.com/admin/commerce/rules/edit_rule.jsp <- look for this tab
-    //         if (tab.url.includes("https://devmcnichols.bigmachines.com/admin/commerce/rules/edit_rule.jsp")) {
-    //             console.log("FOUND CORRECT TAB");
-    //             console.log(tab.id);
-    //             commerceRuleTabID = tab.id;
-    //             commerceRuleTabIndex = tab.index;
-    //             // chrome.tabs.sendMessage(tabs[tab.id], { greeting: "getCommerceFilename" }, function(response) {
-    //             //     console.log(response.commerceFileName);
-    //             // });
-    //             chrome.tabs.sendMessage(result[0].id, { greeting: "getCommerceFilename" }, function(response) { // <-- correct signature but returns error - Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist.
-    //                 // popup.js:187 ;
-    //                 // maybe https://stackoverflow.com/questions/54619817/how-to-fix-unchecked-runtime-lasterror-could-not-establish-connection-receivi
-
-    //                 //try background script for handler
-
-    //                 // chrome.tab.sendMessage docs - continued dev
-
-    //                 console.log("MESSAGE SENT");
-    //                 // console.log(response.commerceFileName);
-    //             });
-    //         }
-    //     });
-
-    //     // MESSAGE COMMERCE RULE TAB CS
-    //     // TODO FIX 
-    //     // Error handling response: TypeError: Error in invocation of tabs.sendMessage(integer tabId, any message, optional object options, optional function responseCallback): No matching signature.
-    //     // at chrome-extension://emnmnbbfkjncmideeepckbclmilhcboc/popup.js:231:21
-    //     // chrome.tabs.sendMessage(result[commerceRuleTabID], { greeting: "getCommerceFilename" }, function(response) {
-    //     //     console.log("MESSAGE SENT");
-    //     //     // console.log(response.commerceFileName);
-    //     // });
-    // });
 
 }
 
@@ -279,14 +115,6 @@ loadButton.addEventListener('click', async(e) => {
     });
 
 });
-
-// let fileHandle;
-// butOpenFile.addEventListener('click', async () => {
-//   [fileHandle] = await window.showOpenFilePicker();
-//   const file = await fileHandle.getFile();
-//   const contents = await file.text();
-//   textArea.value = contents;
-// });
 
 // UNLOAD TEST ONCLICK
 unloadTestButton.onclick = function(params) {
@@ -342,32 +170,3 @@ optionsButton.onclick = function(params) {
     // alert("optionsClicked");
     window.location = '/options.html';
 }
-
-
-// const options = {
-//     types: [
-//       {
-//         description: 'Text Files',
-//         accept: {
-//           'text/plain': ['.txt', '.text'],
-//           'text/html': ['.html', '.htm']
-//         }
-//       },
-//       {
-//         description: 'Images',
-//         accept: {
-//           'image/*': ['.png', '.gif', '.jpeg', '.jpg']
-//         }
-//       }
-//     ],
-
-// const options = {
-//     types: [
-//       {
-//         accept: {
-//           'image/svg+xml': '.svg'
-//         }
-//       },
-//     ],
-//     excludeAcceptAllOption: true
-//   };
