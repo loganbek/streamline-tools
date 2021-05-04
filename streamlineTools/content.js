60b614e732dff589b48bec7415d06b690e2f73f9
Logan Bek <loganbek@gmail.com>
2021-05-04 06:15:22 -0700

    post lint-fix

--- streamlineTools/content.js
+++ streamlineTools/content.js
@@ -1,91 +1,89 @@
 // PRICING + UTIL CONTENT SCRIPT
 // let commentHeader = commentHeader || "";
-var commentHeader = "";
-var code = "";
-var testCode = "";
+let commentHeader = ''
+let code = ''
+let testCode = ''
 
-//Listen for the PassToBackground event
-window.addEventListener("PassToBackground", function(evt) {
-    code = evt.detail;
-}, false);
+// Listen for the PassToBackground event
+window.addEventListener('PassToBackground', function (evt) {
+  code = evt.detail
+}, false)
 
-//Listen for the PassCommentHeader event
-window.addEventListener("PassCommentHeader", function(evt) {
-    commentHeader = evt.detail;
-}, false);
+// Listen for the PassCommentHeader event
+window.addEventListener('PassCommentHeader', function (evt) {
+  commentHeader = evt.detail
+}, false)
 
-//Listen for the code event
-window.addEventListener("PassCodeToBackground", function(evt) {
-    code = evt.detail;
-}, false);
+// Listen for the code event
+window.addEventListener('PassCodeToBackground', function (evt) {
+  code = evt.detail
+}, false)
 
-//Listen for the testcode event
-window.addEventListener("PassTestCodeToBackground", function(evt) {
-    testCode = evt.detail;
-}, false);
+// Listen for the testcode event
+window.addEventListener('PassTestCodeToBackground', function (evt) {
+  testCode = evt.detail
+}, false)
 
-//Listen for the unloadCode event
-window.addEventListener("unloadCode", function(evt) {
-    code = evt.detail;
-}, false);
+// Listen for the unloadCode event
+window.addEventListener('unloadCode', function (evt) {
+  code = evt.detail
+}, false)
 
-function injectJs(link) {
-    let scr = document.createElement('script');
-    scr.type = "text/javascript";
-    scr.src = link;
-    document.getElementsByTagName('head')[0].appendChild(scr);
+function injectJs (link) {
+  const scr = document.createElement('script')
+  scr.type = 'text/javascript'
+  scr.src = link
+  document.getElementsByTagName('head')[0].appendChild(scr)
 }
 
-injectJs(chrome.extension.getURL('injected.js'));
+injectJs(chrome.extension.getURL('injected.js'))
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        let filename = document.getElementById('variableName').value;
-        filename = newFunction(filename);
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        console.log(request.greeting ?
-            "greeting: " + request.greeting :
-            "nogreeting");
-        if (request.greeting == "unload") {
-            let unloadEvent = new CustomEvent("unloadCode", { detail: request.code });
-            window.dispatchEvent(unloadEvent);
-            console.log("CH");
-            console.log(contentHeader);
-            if (!code.startsWith(commentHeader)) {
-                code = commentHeader + "\n\n" + code;
-            }
-            sendResponse({
-                filename: filename,
-                code: code
-            });
-        } else if (request.greeting == "unloadTest") {
-            let unloadTestEvent = new CustomEvent("unloadTestCode", { detail: request.code });
-            window.dispatchEvent(unloadTestEvent);
-            sendResponse({
-                filename: filename,
-                testCode: testCode
-            });
-        } else if (request.greeting == "load") {
-            let loadEvent = new CustomEvent("loadCode", { detail: request.code });
-            window.dispatchEvent(loadEvent);
-        } else if (request.greeting == "loadTest") {
-            console.log(request.code);
-            let loadTestEvent = new CustomEvent("loadTestCode", { detail: request.code });
-            window.dispatchEvent(loadTestEvent);
-        } else if (request.greeting == "filename") {
-            sendResponse({
-                filename: filename
-            });
-        }
-    });
-
-
-
-function newFunction(filename) {
-    if (filename === "") {
-        filename = "nofilename";
+  function (request, sender, sendResponse) {
+    let filename = document.getElementById('variableName').value
+    filename = newFunction(filename)
+    console.log(sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension')
+    console.log(request.greeting
+      ? 'greeting: ' + request.greeting
+      : 'nogreeting')
+    if (request.greeting == 'unload') {
+      const unloadEvent = new CustomEvent('unloadCode', { detail: request.code })
+      window.dispatchEvent(unloadEvent)
+      console.log('CH')
+      console.log(contentHeader)
+      if (!code.startsWith(commentHeader)) {
+        code = commentHeader + '\n\n' + code
+      }
+      sendResponse({
+        filename: filename,
+        code: code
+      })
+    } else if (request.greeting == 'unloadTest') {
+      const unloadTestEvent = new CustomEvent('unloadTestCode', { detail: request.code })
+      window.dispatchEvent(unloadTestEvent)
+      sendResponse({
+        filename: filename,
+        testCode: testCode
+      })
+    } else if (request.greeting == 'load') {
+      const loadEvent = new CustomEvent('loadCode', { detail: request.code })
+      window.dispatchEvent(loadEvent)
+    } else if (request.greeting == 'loadTest') {
+      console.log(request.code)
+      const loadTestEvent = new CustomEvent('loadTestCode', { detail: request.code })
+      window.dispatchEvent(loadTestEvent)
+    } else if (request.greeting == 'filename') {
+      sendResponse({
+        filename: filename
+      })
     }
-    return filename;
+  })
+
+function newFunction (filename) {
+  if (filename === '') {
+    filename = 'nofilename'
+  }
+  return filename
 }
--- streamlineTools/options.js
+++ streamlineTools/options.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
 // Saves options to chrome.storage
-function save_options() {
-    let includeCommentHeader = document.getElementById('includeCommentHeader');
-    chrome.storage.sync.set({
-        includeCommentHeader: includeCommentHeader
-    }, function() {
-        // Update status to let user know options were saved.
-        let status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+function save_options () {
+  const includeCommentHeader = document.getElementById('includeCommentHeader')
+  chrome.storage.sync.set({
+    includeCommentHeader: includeCommentHeader
+  }, function () {
+    // Update status to let user know options were saved.
+    const status = document.getElementById('status')
+    status.textContent = 'Options saved.'
+    setTimeout(function () {
+      status.textContent = ''
+    }, 750)
+  })
 }
 
 // Restores state using the preferences stored in chrome.storage.
-function restore_options() {
-    chrome.storage.sync.get({
-        includeCommentHeader: false
-    }, function(items) {});
+function restore_options () {
+  chrome.storage.sync.get({
+    includeCommentHeader: false
+  }, function (items) {})
 }
 
 if (document.readyState !== 'loading') {
-    // console.log('document is already ready, just execute code here');
-    restore_options();
+  // console.log('document is already ready, just execute code here');
+  restore_options()
 } else {
-    document.addEventListener('DOMContentLoaded', function() {
-        // console.log('document was not ready, place code here');
-        restore_options();
-    });
+  document.addEventListener('DOMContentLoaded', function () {
+    // console.log('document was not ready, place code here');
+    restore_options()
+  })
 }
 
 // document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
-    save_options);
+  save_options)
 
 // BACK HANDLER
-document.getElementById('back').onclick = function(params) {
-    // alert("optionsClicked");
-    window.location = '/popup.html';
+document.getElementById('back').onclick = function (params) {
+  // alert("optionsClicked");
+  window.location = '/popup.html'
 }
