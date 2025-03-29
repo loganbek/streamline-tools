12f0fac80b030d730df812f2f8de97f8ef57f802
Logan Bek <loganbek@gmail.com>
2025-03-29 08:48:12 -0700

    Add commented-out code for Chrome extension testing setup in Puppeteer tests

--- tests/puppeteer/extension.test.js
+++ tests/puppeteer/extension.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 describe('Chrome Extension Tests', () => {
     let browser;
@@ -21,3 +22,37 @@ describe('Chrome Extension Tests', () => {
 
     // Add more tests as needed
 });
+
+// import puppeteer from 'puppeteer';
+// import path from 'path';
+
+// const pathToExtension = path.join(process.cwd(), 'streamline-tools');
+// const browser = await puppeteer.launch({
+//   args: [
+//     `--disable-extensions-except=${pathToExtension}`,
+//     `--load-extension=${pathToExtension}`,
+//   ],
+// });
+
+// const workerTarget = await browser.waitForTarget(
+//   // Assumes that there is only one service worker created by the extension and its URL ends with background.js.
+//   target =>
+//     target.type() === 'service_worker' &&
+//     target.url().endsWith('background.js'),
+// );
+
+// const worker = await workerTarget.worker();
+
+// // Open a popup (available for Canary channels).
+// await worker.evaluate('chrome.action.openPopup();');
+
+// const popupTarget = await browser.waitForTarget(
+//   // Assumes that there is only one page with the URL ending with popup.html and that is the popup created by the extension.
+//   target => target.type() === 'page' && target.url().endsWith('popup.html'),
+// );
+
+// const popupPage = popupTarget.asPage();
+
+// // Test the popup page as you would any other page.
+
+// await browser.close();
